0f891241c33a3134c81021317dce16e0527fbf5e
adrian.rupala <adrian.rupala@whiteaster.com>
2020-05-19 11:23:48 +0200

    add celeary, readis containers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-/app/docker/wait_for.sh harvester_db:27017 -t 5 -- echo "Database (harvester_db) is up!"
+/app/docker/wait_for.sh harvester_db:27017 -t 15 -- echo "Database (harvester_db) is up!"
+/app/docker/wait_for.sh harvester_redis:6379 -t 15 -- echo "Database (harvester_redis) is up!"
 
 cp /app/example.env /app/harvester/.env
 cp /app/example.env /app/.env
