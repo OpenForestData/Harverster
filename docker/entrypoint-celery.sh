3114e55836fb41512de26355c5bcafa75467281b
adrian.rupala <adrian.rupala@whiteaster.com>
2020-05-20 10:02:13 +0200

    replace mongodb to psql

--- docker/entrypoint-celery.sh
+++ docker/entrypoint-celery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/app/docker/wait_for.sh harvester_db:27017 -t 15 -- echo "Database (harvester_db) is up!"
+/app/docker/wait_for.sh harvester_db:5432 -t 15 -- echo "Database (harvester_db) is up!"
 /app/docker/wait_for.sh harvester_redis:6379 -t 15 -- echo "Redis (harvester_redis) is up!"
 
 cp /app/example.env /app/harvester/.env
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-/app/docker/wait_for.sh harvester_db:27017 -t 15 -- echo "Database (harvester_db) is up!"
-/app/docker/wait_for.sh harvester_redis:6379 -t 15 -- echo "Database (harvester_redis) is up!"
+/app/docker/wait_for.sh harvester_db:5432 -t 15 -- echo "Database (harvester_db) is up!"
 
 cp /app/example.env /app/harvester/.env
 cp /app/example.env /app/.env
