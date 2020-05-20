e1b66a9f7c0d5b9ada9c0923fe68d7245208354a
adrian.rupala <adrian.rupala@whiteaster.com>
2020-05-20 10:10:45 +0200

    add harvester_mongo container

--- docker/entrypoint-celery.sh
+++ docker/entrypoint-celery.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /app/docker/wait_for.sh harvester_db:5432 -t 15 -- echo "Database (harvester_db) is up!"
+/app/docker/wait_for.sh harvester_mongo:27017 -t 15 -- echo "Database (harvester_mongo) is up!"
 /app/docker/wait_for.sh harvester_redis:6379 -t 15 -- echo "Redis (harvester_redis) is up!"
 
 cp /app/example.env /app/harvester/.env
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /app/docker/wait_for.sh harvester_db:5432 -t 15 -- echo "Database (harvester_db) is up!"
+/app/docker/wait_for.sh harvester_mongo:27017 -t 15 -- echo "Database (harvester_mongo) is up!"
 
 cp /app/example.env /app/harvester/.env
 cp /app/example.env /app/.env
