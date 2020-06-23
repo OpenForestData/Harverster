0e65c97d55e36b74345eb4eb3bd4868544847601
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-06-23 16:56:25 +0200

    Revert docker/entrypoint file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,5 @@
 cp /app/example.env /app/harvester/.env
 cp /app/example.env /app/.env
 
-python /app/manage.py makemigrations
 python /app/manage.py migrate
 python /app/manage.py runserver 0.0.0.0:8000
