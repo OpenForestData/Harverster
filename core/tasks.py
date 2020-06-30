48490584527d5bdb64c77348e3061f00944203cd
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-06-30 18:42:45 +0200

    Add logger debugs and doctring to tasks file

--- core/tasks.py
+++ core/tasks.py
@@ -11,21 +11,27 @@ logger = logging.getLogger(__name__)
 
 
 @shared_task()
-def run_harvester(name):
+def run_harvester(name: str) -> None:
     """
     Using designated client harvests data form specified system
+
+    :param name: client name
     """
-    logger.debug("Started run harvest function")
+    logger.debug(f"Starting run harvest function for {name}")
     dataverse_client = Api(settings.DATAVERSE_URL, settings.DATAVERSE_API_KEY)
     app_client = get_client(name)
 
     harvester = HarvestingController(app_client, dataverse_client)
 
     add_data, modify_data, remove_data = harvester.run_harvest()
+    logger.debug(f"Harvested data from source of {name}")
 
     if add_data:
+        logger.debug(f"Starting adding new resources from {name}")
         harvester.add_resources(add_data)
     if modify_data:
+        logger.debug(f"Starting updating resources from {name}")
         harvester.update_resources(modify_data)
     if remove_data:
+        logger.debug(f"Starting removing resources from {name}")
         harvester.delete_resources(remove_data)
