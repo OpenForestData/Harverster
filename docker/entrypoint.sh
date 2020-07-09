0c357ef71b2811c77360aa75375003665d00a208
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-07-09 14:26:31 +0200

    Changed chmod for entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,6 @@
 
 /app/docker/wait_for.sh harvester_db:5432 -t 15 -- echo "Database (harvester_db) is up!"
 
+python /app/manage.py makemigrations
 python /app/manage.py migrate
 python /app/manage.py runserver 0.0.0.0:8000
