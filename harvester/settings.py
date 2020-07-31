041a4c8cd5fd8be5c0e99565e3579345b24c4e41
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-07-31 08:32:24 +0200

    Client dict use now enviroments

--- harvester/settings.py
+++ harvester/settings.py
@@ -141,19 +141,19 @@ CLIENTS_DICT = {
     'geonode': {
         'module': 'adapters.geonode.client',
         'class': 'GeonodeClient',
-        'url': 'https://gis.openforestdata.pl/',
-        'api_key': None
+        'url': os.getenv('GEONODE_API_KEY', None),
+        'api_key': os.getenv('GEONODE_API_KEY', None)
     },
     'grafana': {
         'module': 'adapters.grafana.client',
         'class': 'GrafanaClient',
-        'url': 'http://192.168.1.97:3000',
-        'api_key': 'eyJrIjoiRUxabFFRbE9saDRZamxFZHVtV3lJblM5MFExaG1BZ0giLCJuIjoiVGVzdCIsImlkIjoxfQ=='
+        'url': os.getenv('GRAFANA_API_KEY', None),
+        'api_key': os.getenv('GRAFANA_API_KEY', None)
     },
     'orthanc': {
         'module': 'adapters.orthanc.client',
         'class': 'OrthancClient',
-        'url': 'http://212.33.83.34/',
-        'api_key': None
+        'url': os.getenv('ORTHANC_API_KEY', None),
+        'api_key': os.getenv('ORTHANC_API_KEY', None)
     }
 }
