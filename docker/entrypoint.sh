d8637d386049a2780dd93d10f42d89d06669df16
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-09-08 16:35:42 +0200

    Fix static files and add STATIC_ROOT

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 python /app/manage.py migrate
+python /app/manage.py collectstatic --noinput
 
 /usr/local/bin/gunicorn harvester.wsgi -b 0.0.0.0:8000
--- harvester/settings.py
+++ harvester/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = '/static-backend/'
 STATIC_URL = '/static-backend/'
 
 EXTERNAL_FILES_ROOT = '/tmp/'
